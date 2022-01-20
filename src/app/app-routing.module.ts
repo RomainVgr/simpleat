d495bcc342775610910f908f0ef11f3546085055
Romain <51331158+zylfu@users.noreply.github.com>
2022-01-20 15:20:37 +0100

    -avancement page filters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ListCategoriesComponent } from './pages/list-categories/list-categories.component';
 import { RestoPageComponent } from './pages/resto-page/resto-page.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { FiltersPageComponent } from './pages/filters-page/filters-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
     { path: 'categories', component: ListCategoriesComponent },
     { path: 'favoris', component: FavorisUserComponent },
+    { path: 'filters', component: FiltersPageComponent },
     { path: 'Deconnexion', redirectTo: 'home'},
     {path: 'restaurants',component: RestoPageComponent},
     {path: 'page-not-found',component: PageNotFoundComponent},
