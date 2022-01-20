659637348d3eb8c0f84b663127a8c85f4f3f4d3f
Romain <51331158+zylfu@users.noreply.github.com>
2022-01-20 11:27:07 +0100

    Maj header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './header/components/nav-bar/nav-bar.component';
 import { SearchBarComponent } from './header/components/search-bar/search-bar.component';
-import { PriceRestoComponent } from './filter/components/price-resto/price-resto.component';
-import { DistanceRestoComponent } from './filter/components/distance-resto/distance-resto.component';
-import { SurPlaceAEmporterComponent } from './filter/components/sur-place-a-emporter/sur-place-a-emporter.component';
-import { PmrRestoComponent } from './filter/components/pmr-resto/pmr-resto.component';
-import { AvisRestoComponent } from './filter/components/avis-resto/avis-resto.component';
 import { CardCategoryComponent } from './card-category/card-category.component';
 import { CardRestoComponent } from './card-resto/card-resto.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ListCategoriesComponent } from './pages/list-categories/list-categories.component';
-import { FavorisUserComponent } from './pages/favoris-user/favoris-user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SearchBarComponent,
-    PriceRestoComponent,
-    DistanceRestoComponent,
-    SurPlaceAEmporterComponent,
-    PmrRestoComponent,
-    AvisRestoComponent,
     CardCategoryComponent,
     CardRestoComponent,
     FooterComponent,
     HomePageComponent,
-    ListCategoriesComponent,
-    FavorisUserComponent
+    ListCategoriesComponent
   ],
   imports: [
     BrowserModule,
