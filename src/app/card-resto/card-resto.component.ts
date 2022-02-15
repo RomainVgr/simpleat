993be2e1b0089d791a74bd71107bf0547333ce34
Thomas Cardon <thomas.cardon35@gmail.com>
2022-02-15 14:54:17 +0100

    distance+Site Web in cardResto

--- src/app/card-resto/card-resto.component.ts
+++ src/app/card-resto/card-resto.component.ts
@@ -8,13 +8,45 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardRestoComponent implements OnInit {
 
   @Input() restaurant : any ; 
+  distance : number;
   
-  constructor() { }
+  constructor() {
+    this.distance = 0 ;
+   }
 
   ngOnInit(): void {
 
     console.log(this.restaurant);
+
+    this.distance = Math.round(
+      this.getDistanceFromLatLonInKm(
+      48.86201110271593 , //latitude Simplon
+      2.4361804827725417, //longitude Simplon
+      this.restaurant.latitude,
+      this.restaurant.longitude)
+      );
+
+    console.log(this.distance);
+      
     
+  }
+
+  getDistanceFromLatLonInKm(lat1 : number , lon1 : number, lat2 : number, lon2 : number) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c * 1000; // Distance in meters
+    return d;
+  }
+  
+  deg2rad(deg : number) {
+    return deg * (Math.PI/180)
   }
 
 
