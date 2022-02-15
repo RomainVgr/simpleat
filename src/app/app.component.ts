35d7393eaf84e469983d5d8a27c96bf9630c62da
Vincent Ramiere <vincent.ramiere1@gmail.com>
2022-02-15 15:30:33 +0100

    fusion apres trompage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simpleat';
+  constructor(private router:Router){
+    this.router.events.subscribe(e=>{
+      //console.log(e);
+      if(e instanceof NavigationEnd){
+        console.log(e.url)
+      }
+    })
+  }
 }
