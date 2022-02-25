4d92ddb960e2e95dbf98ce3b1ac0bd5e8f1c5e09
Thomas Cardon <thomas.cardon35@gmail.com>
2022-02-25 16:43:31 +0100

    search-bar = ok

--- src/app/header/components/search-bar/search-bar.component.ts
+++ src/app/header/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { RestoPageComponent } from 'src/app/pages/resto-page/resto-page.component';
 import { ApiBackService } from 'src/app/services/api-back.service';
 
 @Component({
@@ -35,7 +36,8 @@ export class SearchBarComponent implements OnInit {
       console.log(this.restauByName);
 
       this.apiBackService.setListRestau(this.restauByName, "filtres");
+      this.route.routeReuseStrategy.shouldReuseRoute= () => false;
+      this.route.onSameUrlNavigation = 'reload';
       this.route.navigate(['restaurants']);
     }
   }
-
